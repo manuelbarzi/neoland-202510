636d27ce07bc07d1f243ca8715875195b47a3113
manuelbarzi <manuelbarzi@gmail.com>
2025-11-10 21:48:04 +0100

    add collection js

---
+++ staff/manuel-barzi/playground/js/collection.js
@@ -0,0 +1,12 @@
+var collection = {
+    count: 0
+}
+
+// TODO implement method add
+
+collection.add('Peter')
+collection.add('Wendy')
+collection.add('James')
+
+console.log(collection)
+// { 0: 'Peter', 1: 'Wendy', 2: 'James', count: 3 }
