6203a5b47f9c1a8abbfe8795cb2ff07e797d4ffa
manuelbarzi <manuelbarzi@gmail.com>
2025-12-01 20:36:10 +0100

    add style for and id attributes to active elements (inputs and buttons); link labels with inputs; add initial styles to landing and register (to be continued)

--- staff/manuel-barzi/product/zzz/app.1/main.js
+++ staff/manuel-barzi/product/zzz/app.1/main.js
@@ -1,14 +1,9 @@
-// body
-
-document.body.className = 'p-4 border border-4 border-red-500 h-screen'
-
 // landing
 
 const landingView = document.createElement('div')
 
 const landingTitle = document.createElement('h1')
 landingTitle.textContent = 'MyPet'
-landingTitle.className = 'font-bold'
 landingView.appendChild(landingTitle)
 
 const landingWelcome = document.createElement('p')
@@ -19,14 +14,12 @@ const landingAccess = document.createElement('nav')
 const landingLoginLink = document.createElement('a')
 landingLoginLink.textContent = 'Login'
 landingLoginLink.href = ''
-landingLoginLink.className = 'underline'
 landingAccess.appendChild(landingLoginLink)
 const landingOrText = document.createTextNode(' or ')
 landingAccess.appendChild(landingOrText)
 const landingRegisterLink = document.createElement('a')
 landingRegisterLink.textContent = 'Register'
 landingRegisterLink.href = ''
-landingRegisterLink.className = 'underline'
 landingAccess.appendChild(landingRegisterLink)
 landingView.appendChild(landingAccess)
 
@@ -53,92 +46,40 @@ registerView.style.display = 'none'
 
 const registerTitle = document.createElement('h1')
 registerTitle.textContent = 'MyPet'
-registerTitle.className = 'font-bold'
 registerView.appendChild(registerTitle)
 
 const registerSubtitle = document.createElement('h2')
 registerSubtitle.textContent = 'Register'
-registerSubtitle.className = 'font-bold'
 registerView.appendChild(registerSubtitle)
 
 const registerForm = document.createElement('form')
 const registerNameLabel = document.createElement('label')
 registerNameLabel.textContent = 'Name'
-registerNameLabel.htmlFor = 'name'
 registerForm.appendChild(registerNameLabel)
 const registerNameInput = document.createElement('input')
-registerNameInput.id = 'name'
-registerNameInput.type = 'text'
 registerForm.appendChild(registerNameInput)
 const registerEmailLabel = document.createElement('label')
 registerEmailLabel.textContent = 'Email'
-registerEmailLabel.htmlFor = 'email'
 registerForm.appendChild(registerEmailLabel)
 const registerEmailInput = document.createElement('input')
-registerEmailInput.id = 'email'
-registerEmailInput.type = 'email'
 registerForm.appendChild(registerEmailInput)
 const registerUsernameLabel = document.createElement('label')
 registerUsernameLabel.textContent = 'Username'
-registerUsernameLabel.htmlFor = 'username'
 registerForm.appendChild(registerUsernameLabel)
 const registerUsernameInput = document.createElement('input')
-registerUsernameInput.id = 'username'
-registerUsernameInput.type = 'text'
 registerForm.appendChild(registerUsernameInput)
 const registerPasswordLabel = document.createElement('label')
-registerPasswordLabel.htmlFor = 'password'
 registerPasswordLabel.textContent = 'Password'
 registerForm.appendChild(registerPasswordLabel)
 const registerPasswordInput = document.createElement('input')
-registerPasswordInput.id = 'password'
-registerPasswordInput.type = 'password'
 registerForm.appendChild(registerPasswordInput)
-const registerShowPasswordButton = document.createElement('button')
-registerShowPasswordButton.textContent = 'Show'
-registerShowPasswordButton.type = 'button'
-registerForm.appendChild(registerShowPasswordButton)
-
-registerShowPasswordButton.addEventListener('click', function(event) {
-    event.preventDefault()
-
-    if (registerPasswordInput.type === 'password') {
-        registerPasswordInput.type = 'text'
-        registerShowPasswordButton.textContent = 'Hide'
-    } else {
-        registerPasswordInput.type = 'password'
-        registerShowPasswordButton.textContent = 'Show'
-    }
-})
-
 const registerPasswordRepeatLabel = document.createElement('label')
 registerPasswordRepeatLabel.textContent = 'Repeat Password'
-registerPasswordRepeatLabel.htmlFor = 'passwordRepeat'
 registerForm.appendChild(registerPasswordRepeatLabel)
 const registerPasswordRepeatInput = document.createElement('input')
-registerPasswordRepeatInput.id = 'passwordRepeat'
-registerPasswordRepeatInput.type = 'password'
 registerForm.appendChild(registerPasswordRepeatInput)
-const registerShowPasswordRepeatButton = document.createElement('button')
-registerShowPasswordRepeatButton.textContent = 'Show'
-registerShowPasswordRepeatButton.type = 'button'
-registerForm.appendChild(registerShowPasswordRepeatButton)
-
-registerShowPasswordRepeatButton.addEventListener('click', function(event) {
-    event.preventDefault()
-
-    if (registerPasswordRepeatInput.type === 'password') {
-        registerPasswordRepeatInput.type = 'text'
-        registerShowPasswordRepeatButton.textContent = 'Hide'
-    } else {
-        registerPasswordRepeatInput.type = 'password'
-        registerShowPasswordRepeatButton.textContent = 'Show'
-    }
-})
-
 const registerSubmitButton = document.createElement('button')
 registerSubmitButton.textContent = 'Register'
-registerSubmitButton.type = 'submit'
 registerForm.appendChild(registerSubmitButton)
 registerView.appendChild(registerForm)
 
@@ -197,40 +138,16 @@ loginView.appendChild(loginSubtitle)
 const loginForm = document.createElement('form')
 const loginUsernameLabel = document.createElement('label')
 loginUsernameLabel.textContent = 'Username'
-loginUsernameLabel.htmlFor = 'username'
 loginForm.appendChild(loginUsernameLabel)
 const loginUsernameInput = document.createElement('input')
-loginUsernameInput.id = 'username'
-loginUsernameInput.type = 'text'
 loginForm.appendChild(loginUsernameInput)
 const loginPasswordLabel = document.createElement('label')
 loginPasswordLabel.textContent = 'Password'
-loginPasswordLabel.htmlFor = 'password'
 loginForm.appendChild(loginPasswordLabel)
 const loginPasswordInput = document.createElement('input')
-loginPasswordInput.id = 'password'
-loginPasswordInput.type = 'password'
 loginForm.appendChild(loginPasswordInput)
-const loginShowPasswordButton = document.createElement('button')
-loginShowPasswordButton.textContent = 'Show'
-loginShowPasswordButton.type = 'button'
-loginForm.appendChild(loginShowPasswordButton)
-
-loginShowPasswordButton.addEventListener('click', function(event) {
-    event.preventDefault()
-
-    if (loginPasswordInput.type === 'password') {
-        loginPasswordInput.type = 'text'
-        loginShowPasswordButton.textContent = 'Hide'
-    } else {
-        loginPasswordInput.type = 'password'
-        loginShowPasswordButton.textContent = 'Show'
-    }
-})
-
 const loginSubmitButton = document.createElement('button')
 loginSubmitButton.textContent = 'Login'
-loginSubmitButton.type = 'submit'
 loginForm.appendChild(loginSubmitButton)
 loginView.appendChild(loginForm)
 
