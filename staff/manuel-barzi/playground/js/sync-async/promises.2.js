db3e655dd081e5e8af99e73b0de2d6f449c724f5
manuelbarzi <manuelbarzi@gmail.com>
2026-02-05 22:20:03 +0100

    add more promises demos

--- staff/manuel-barzi/playground/js/sync-async/promises.2.js
+++ staff/manuel-barzi/playground/js/sync-async/promises.2.js
@@ -1,11 +1,10 @@
 const log = message => console.log('%c' + new Date().toISOString() + ' %c' + message, 'color: greenyellow', 'color: tomato')
 
 log('start')
-    
+
 new Promise((resolve, reject) => {
     setTimeout(() => {
-        //resolve(10)
-        reject(new Error(10))
+        resolve(10)
 
         log(10)
     }, 1000)
@@ -14,7 +13,6 @@ new Promise((resolve, reject) => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(value + 10)
-                //reject(new Error(value + 10))
 
                 log(value + 10)
             }, 2000)
@@ -29,7 +27,4 @@ new Promise((resolve, reject) => {
             }, 3000)
         })
     })
-    .catch(error => log(error))
     .then(() => log('end'))
-
-log('wait for queued callbacks')
