fc4ebb84153ece3623321e2ab0e67dfc3f77ffa6
manuelbarzi <manuelbarzi@gmail.com>
2026-02-27 20:41:59 +0100

    update error handling in app; use custom errors and apply specific feedback in register and login

--- staff/manuel-barzi/product/app/views/Login.jsx
+++ staff/manuel-barzi/product/app/views/Login.jsx
@@ -8,6 +8,7 @@ import { Anchor } from './components/commons/Anchor'
 import { Feedback } from './components/commons/Feedback'
 
 import { logic } from '../logic'
+import { CredentialError, ExistenceError, ValidationError } from '../errors'
 
 export function Login({ onUserLoggedIn, onGoToRegister }) {
     console.log('Login -> call')
@@ -25,9 +26,19 @@ export function Login({ onUserLoggedIn, onGoToRegister }) {
         try {
             logic.loginUser(username, password)
                 .then(() => onUserLoggedIn())
-                .catch(error => setFeedback({ message: error.message, level: 'error' }))
+                .catch(error => {
+                    if (error instanceof ValidationError)
+                        setFeedback({ message: error.message, level: 'warn' })
+                    else if (error instanceof ExistenceError || error instanceof CredentialError)
+                        setFeedback({ message: error.message, level: 'danger' })
+                    else
+                        setFeedback({ message: 'sorry, something failed. try again later', level: 'error' })
+                })
         } catch (error) {
-            setFeedback({ message: error.message, level: 'error' })
+            if (error instanceof ValidationError)
+                setFeedback({ message: error.message, level: 'warn' })
+            else
+                setFeedback({ message: 'sorry, something failed. try again later', level: 'error' })
         }
     }
 
--- staff/manuel-barzi/product/app/views/Register.jsx
+++ staff/manuel-barzi/product/app/views/Register.jsx
@@ -8,6 +8,7 @@ import { Anchor } from './components/commons/Anchor'
 import { Feedback } from './components/commons/Feedback'
 
 import { logic } from '../logic'
+import { DuplicityError, ValidationError } from '../errors'
 
 export function Register({ onGoToLogin }) {
     console.log('Register -> call')
@@ -34,9 +35,19 @@ export function Register({ onGoToLogin }) {
 
                     onGoToLogin()
                 })
-                .catch(error => setFeedback({ message: error.message, level: 'error' }))
+                .catch(error => {
+                    if (error instanceof ValidationError)
+                        setFeedback({ message: error.message, level: 'warn' })
+                    else if (error instanceof DuplicityError)
+                        setFeedback({ message: error.message, level: 'danger' })
+                    else
+                        setFeedback({ message: 'sorry, something failed. try again later', level: 'error' })
+                })
         } catch (error) {
-            setFeedback({ message: error.message, level: 'error' })
+            if (error instanceof ValidationError)
+                setFeedback({ message: error.message, level: 'warn' })
+            else
+                setFeedback({ message: 'sorry, something failed. try again later', level: 'error' })
         }
     }
 
