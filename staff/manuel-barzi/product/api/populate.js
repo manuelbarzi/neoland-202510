f1e2afb52e769021d2f4cfbcc8ad87ffe95aaa64
manuelbarzi <manuelbarzi@gmail.com>
2026-03-09 22:43:20 +0100

    add mongodb client and use it in populate

--- staff/manuel-barzi/product/api/populate.js
+++ staff/manuel-barzi/product/api/populate.js
@@ -1,20 +1,36 @@
-import { data, User, Pet } from './data.js'
+import { MongoClient, ObjectId } from 'mongodb'
 
-data.insertUser(new User('user-' + data.usersCount, 'Peter Pan', 'peter@pan.com', 'peterpan', '123123123', null,'regular'))
-data.insertUser(new User('user-' + data.usersCount, 'Wendy Darling', 'wendy@darling.com', 'wendydarling', '123123123', null, 'regular'))
-data.insertUser(new User('user-' + data.usersCount, 'Campa Nilla', 'campa@nilla.com', 'campanilla', '123123123', null, 'regular'))
-data.insertUser(new User('user-' + data.usersCount, 'Pepito Grillo', 'pepito@grillo.com', 'pepitogrillo', '123123123', null, 'regular'))
+const client = new MongoClient('mongodb://localhost:27017')
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Sultan', '2024-12-01', 20, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTVhMDRuNzh2c2Z4cmxzN2U0aHRsOTFzMDBvcnoydG1wZTc4dXBnbSZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/FY8c5SKwiNf1EtZKGs/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Simba', '2020-12-01', 3, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXM3YjY4aDF6bHhtN29lZDlxYzBvZDRuOGFtN3ZoYmt6MHVzMXYzeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/vyBYO8ENb4eDFpYmDI/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-0', 'Donatello', '2014-12-01', 2, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnNtenlmajY2Yzd3aTFmcTh0YXA0Ymw5emVpYmY0NDV3czJ1NWFydiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/65AMOI22i27d34VBdM/giphy.gif'))
+client.connect()
+    .then(() => {
+        const db = client.db('product')
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-1', 'Mafia', '2025-07-01', 1.5, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnI3Z3J0c3lybjFwcmVpMjVoN29nNXQzNml2ZTNjbXZ3NHdncTQ5MCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/901mxGLGQN2PyCQpoc/giphy.gif'))
+        const users = db.collection('users')
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-2', 'Dormilon', '2019-11-01', 4, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnI3Z3J0c3lybjFwcmVpMjVoN29nNXQzNml2ZTNjbXZ3NHdncTQ5MCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/v6aOjy0Qo1fIA/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-2', 'Espumita', '2024-10-01', 1.4, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGdmajV3cHltazV2Z2ttZGs0bXp3YjZ0dTRmamUwNWNuYTM2a2UyeiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/cYZkY9HeKgofpQnOUl/giphy.gif'))
+        // users.find({}).toArray()
+        //     .then(users => console.table(users))
+        //     .catch(error => console.error(error))
 
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Osito', '2023-09-01', 3.5, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHQ3b2NjNDE3aW1rZGUwYTJsaXI4dzV6aGI5cGk0NmE4aGJ2cmhoMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lNLqexL939DTyR0uH2/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Negrita', '2022-06-01', 3.2, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHQ3b2NjNDE3aW1rZGUwYTJsaXI4dzV6aGI5cGk0NmE4aGJ2cmhoMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Q60eJgzLbQUM6qFRQo/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Sargento', '2022-09-01', .2, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzVpOXp6ZGRlNXdwdHh3Z28xODRzN3djMTl2ZTF2MHY1NGl4dW13NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3imh62nmqnKYutIGnJ/giphy.gif'))
-data.insertPet(new Pet('pet-' + data.petsCount, 'user-3', 'Egoista', '2023-09-01', .25, 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzVpOXp6ZGRlNXdwdHh3Z28xODRzN3djMTl2ZTF2MHY1NGl4dW13NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/D74yIoiOstPA1e3xYL/giphy.gif'))
+        // users.insertOne({ name: 'Le Chuga', email: 'le@chuga.com', username: 'lechuga', password: '123123123' })
+        //     .then(result => console.log(result))
+        //     .catch(error => console.error(error))
+
+        // users.updateOne({ _id: new ObjectId('69af22954d59c13ca7bf6889') }, { $set: { password: '345345345' } })
+        //     .then(result => console.log(result))
+        //     .catch(error => console.error(error))
+
+        // users.deleteOne({ _id: new ObjectId('69af23f64d59c13ca7bf688b') })
+        //     .then(result => console.log(result))
+        //     .catch(error => console.error(error))
+
+        // users.findOne({ _id: new ObjectId('69af23424d59c13ca7bf688f') })
+        //     .then(user => console.log(user))
+        //     .catch(error => console.error(error))
+
+        users.find({ name: /l/i }).toArray()
+            //.then(users => console.table(users))
+            .then(users => console.log(users))
+            .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error))
