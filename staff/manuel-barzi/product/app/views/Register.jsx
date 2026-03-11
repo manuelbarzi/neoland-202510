b4cf436eddcc8e8a8e18a8451cdc1b0731af1e0d
manuelbarzi <manuelbarzi@gmail.com>
2026-03-11 20:57:19 +0100

    correct register view in app; use mongoose in data in api and adapt logic and interface to data with promises

--- staff/manuel-barzi/product/app/views/Register.jsx
+++ staff/manuel-barzi/product/app/views/Register.jsx
@@ -26,8 +26,6 @@ export function Register({ onGoToLogin, onError }) {
                 .then(() => {
                     form.reset()
 
-                    setFeedback(null)
-
                     onGoToLogin()
                 })
                 .catch(error => onError(error))
