fc827c1b4d31382192010bbce1c8e56cedbefef8
manuelbarzi <manuelbarzi@gmail.com>
2026-03-18 20:25:50 +0100

    add key in each item in pet-list for react efficiency

--- staff/manuel-barzi/product/app/views/components/PetList.jsx
+++ staff/manuel-barzi/product/app/views/components/PetList.jsx
@@ -61,7 +61,7 @@ export function PetList({ onGoToPetDetail }) {
     return <div>
         <ul className="flex flex-col gap-2 mt-2">
             {pets.map(pet =>
-                <li className="flex items-center border-2 border-black p-2 justify-between" onClick={() => handleGoToPetDetailClick(pet.id)}>
+                <li key={pet.id} className="flex items-center border-2 border-black p-2 justify-between" onClick={() => handleGoToPetDetailClick(pet.id)}>
                     <div className="flex items-center gap-4">
                         <img src={pet.image} className="rounded-full w-10 h-10 object-cover" />
 
