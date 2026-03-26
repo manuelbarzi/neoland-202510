de8a5ebf3b87a5f91190cd46feb411e21bb322e7
manuelbarzi <manuelbarzi@gmail.com>
2026-03-26 22:05:00 +0100

    split models data and logic in api into separate files per symbols

--- staff/manuel-barzi/product/api/index.js
+++ staff/manuel-barzi/product/api/index.js
@@ -3,10 +3,10 @@ import cors from 'cors'
 import morganBody from 'morgan-body'
 import jwt from 'jsonwebtoken'
 
-import { logic } from './logic.js'
+import { logic } from './logic/index.js'
 import { DuplicityError, ExistenceError, OwnershipError, SystemError, ValidationError, CredentialError, AuthError } from 'com'
 
-import { database } from './models.js'
+import { database } from './models/index.js'
 
 database.connect(process.env.DB_URL)
     .then(() => {
--- staff/manuel-barzi/product/com/validate.js
+++ staff/manuel-barzi/product/com/validate.js
@@ -1,9 +1,5 @@
-import { ValidationError } from "./errors.js"
-
-const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-const URL_REGEX = /(www|http:|https:)+[^\s]+[\w]/
-const ISODATE_REGEX = /^\d{4}-\d{2}-\d{2}$/
-const ID_REGEX = /^[0-9a-fA-F]{24}$/
+import { ValidationError } from './errors.js'
+import { EMAIL_REGEX, URL_REGEX, ISODATE_REGEX, ID_REGEX } from './regex.js'
 
 class Validate {
     name(name) {
